6bbd3b93b93b5f4b3471ab79d61175add31c29be
Jung HaEun <codingha00@gmail.com>
2025-06-05 17:52:09 +0900

    [feat] 토큰 로그 출력

--- src/main/java/com/finalproject/backend/controller/UserController.java
+++ src/main/java/com/finalproject/backend/controller/UserController.java
@@ -2,6 +2,9 @@ package com.finalproject.backend.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +17,12 @@ import java.util.Map;
 public class UserController {
     @SecurityRequirement(name = "github-oauth")
     @GetMapping("/user/me")
-    public Map<String,Object> user(@AuthenticationPrincipal OAuth2User principal) {
+    public Map<String,Object> user(
+            @AuthenticationPrincipal OAuth2User principal,
+            @RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient client
+    ) {
+        OAuth2AccessToken accessToken = client.getAccessToken();
+        System.out.println("🔑 Access Token: " + accessToken.getTokenValue());
         return principal.getAttributes();
     }
 }
