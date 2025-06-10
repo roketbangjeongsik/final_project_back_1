3dc6c5f315a8e2ce672062819867ea350c3836f7
hyeji <wedgejjangdol@gmail.com>
2025-06-10 18:58:21 +0900

    로그인 필드 반환 수정 및 로그아웃 api 추가

--- src/main/java/com/finalproject/backend/controller/UserController.java
+++ src/main/java/com/finalproject/backend/controller/UserController.java
@@ -1,12 +1,20 @@
 package com.finalproject.backend.controller;
 
+import com.finalproject.backend.dto.user.UserResponse;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,14 +23,32 @@ import java.util.Map;
 @RequestMapping("/api/v1")
 @RestController
 public class UserController {
-    @SecurityRequirement(name = "github-oauth")
     @GetMapping("/user/me")
-    public Map<String,Object> user(
-            @AuthenticationPrincipal OAuth2User principal,
-            @RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient client
+
+    public UserResponse user(
+            @Parameter(hidden = true)
+            @AuthenticationPrincipal OAuth2User principal
     ) {
-        OAuth2AccessToken accessToken = client.getAccessToken();
-        System.out.println("🔑 Access Token: " + accessToken.getTokenValue());
-        return principal.getAttributes();
+        String login     = principal.getAttribute("login");
+        String name      = principal.getAttribute("name");
+        String avatarUrl = principal.getAttribute("avatar_url");
+
+        return new UserResponse(login, name, avatarUrl);
+    }
+    @PostMapping("/user/logout")
+    public ResponseEntity<Void> logout(
+            @Parameter(hidden = true)
+            HttpServletRequest request,
+            @Parameter(hidden = true)
+            HttpServletResponse response) {
+
+        request.getSession().invalidate();
+        SecurityContextHolder.clearContext();
+        Cookie cookie = new Cookie("JSESSIONID", null);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/com/finalproject/backend/dto/user/UserResponse.java
@@ -0,0 +1,14 @@
+package com.finalproject.backend.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserResponse {
+    private String login;
+    private String name;
+    @JsonProperty("avatar_url")
+    private String avatarUrl;
+}
